7a91be9914ac7ce4a46124d47444cfba77191252
Alex Peshkov <alp@aboutecho.com>
2013-03-17 00:40:31 +0400

    [*] Fixed parameters sequence in handle_call

--- src/ttt_srv.erl
+++ src/ttt_srv.erl
@@ -16,29 +16,32 @@ handle_call({new, AuthorKey}, _From, GameList) ->
   ets:insert_new(GameList, {Pid, {pair_waiting, AuthorKey}}),
   Reply = {ok, Pid},
   {reply, Reply, GameList};
-handle_call({join, Pid, Key}, _From, GameList) ->
-  Reply = case ets:lookup(GameList, Pid) of
-    [{Pid, {pair_waiting, Key}}]  -> {ok, already_joined};
-    [{Pid, {pair_waiting, AuthorKey}}] -> ets:delete(GameList, Pid), 
-                                  ets:insert_new(GameList, {Pid, {game_ready, AuthorKey, Key}}), 
-                                  {ok, joined};
-    [{Pid, {game_ready, _, _}}] -> {error, players_exceeded};
-    _ -> {error, game_desnt_exist}
-  end,
-  {reply, Reply, GameList};   
-handle_call({move, Coords, Key, Game}, _From, GameList) -> 
+
+handle_call({join, Game, Key}, _From, GameList) ->
   Reply = case ets:lookup(GameList, Game) of
-    {Game, {game_ready, Key1, Key2}} -> 
+    [{Game, {pair_waiting, Key}}]  -> {ok, already_joined};
+    [{Game, {pair_waiting, AuthorKey}}] -> ets:delete(GameList, Game), 
+                                  ets:insert_new(GameList, {Game,  {game_ready, AuthorKey, Key}}), 
+                                  {ok, joined};
+    [{Game, {game_ready, _, _}}] -> {error, players_exceeded};
+    [] -> {error, game_doesnt_exist}
+  end,
+  {reply, Reply, GameList}; 
+  
+handle_call({move, Coords, Game, Key}, _From, GameList) -> 
+  Reply = case ets:lookup(GameList, Game) of
+    [{Game, {game_ready, Key1, Key2}}] -> 
       OpKey = get_opponent({Key1, Key2}, Key),
       case gen_server:call(Game, {move, Coords, Key}) of
-        {ok, {X,Y}} ->  OpKey! {moved, {X, Y}}, {ok, {X,Y}}; 
-        {ok, win} -> Key! {ok, won}, OpKey! {ok, lost}, {ok, win}
+        {ok, {X, Y}} -> get_pid(OpKey)!{moved, {X, Y}}, {ok, {X,Y}}; 
+        {ok, win} ->  get_pid(Key)!{ok, won}, get_pid(OpKey)!{ok, lost}, {ok, win}
       end; 
-    _ -> {error, game_desnt_exist} 
+    [] -> {error, game_doesnt_exist} 
   end,
   {reply, Reply, GameList};
+
 handle_call({games}, _From, GameList) ->
-  Reply = ets:all(GameList),
+  Reply = ets:tab2list(GameList),
   {reply, Reply, GameList}.
 
 handle_cast(_Msg, State) -> {noreply, State}.
@@ -49,5 +52,8 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 get_opponent({Key1, Key2}, Key) when Key1 == Key -> Key2;
 get_opponent({Key1, Key2}, Key) when Key2 == Key -> Key1.
     
-
+get_pid(Pid) when is_list(Pid) -> list_to_pid(Pid);
+get_pid(Pid) when is_binary(Pid) -> list_to_pid(binary_to_list(Pid));
+get_pid(Pid) when is_atom(Pid) -> list_to_pid(atom_to_list(Pid));
+get_pid(Pid) -> Pid.
 
